b410cab168e5ddb4d2521b56963145e0337a0533
Indu Chaube <Indu Chaube>
2016-09-11 14:20:59 -0700

    Convert string in array with the matching separator, test program

--- index.php
+++ index.php
@@ -19,3 +19,12 @@ echo $cal_obj->add_num(1,2);
 
 //END of files
 ?>
+
+<?php
+//Convert string in array with the matching separator.
+//Explode in php
+$str = "My name is Indu Prakash";
+
+//exploding along with space and printing array
+print_r (explode(" ",$str));
+?>
